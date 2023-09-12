c802560fe0acc418ee0029d38ba7e919edb00927
emse-p4a-gwu <john.helveston@gmail.com>
2023-09-12 13:57:03 -0400

    made w3 notes files

--- class/render.R
+++ class/render.R
@@ -12,6 +12,12 @@ tools::compactPDF(paste0(lesson, ".pdf"), gs_quality = 'ebook')
 zip::zip(
     zipfile = paste0(lesson, ".zip"),
     files = c(
-        
+        "quarto_demo.qmd",
+        "bears_solutions.qmd",
+        "bears.qmd",
+        "data",
+        "ggplot2_solutions.qmd",
+        "ggplot2.qmd",
+        "logo.png",
         paste0(lesson, ".Rproj"))
 )
